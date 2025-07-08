5117cf0cf3a1622a1f7f817bce6c3952352cc668
WegenerSteven <wegenersteven580@gmail.com>
2025-07-08 19:23:17 +0300

    add sonner ui component

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,30 @@
+import { Toaster as Sonner } from "sonner"
+
+interface ToasterProps {
+  theme?: "light" | "dark" | "system" | undefined
+  className?: string
+  toastOptions?: any
+}
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  return (
+    <Sonner
+      theme="light"
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
