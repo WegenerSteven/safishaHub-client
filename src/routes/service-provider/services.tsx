215c50881a9b235ac01aaec1b60107299343c3d9
WegenerSteven <wegenersteven580@gmail.com>
2025-07-13 23:40:09 +0300

    remove duplicate files

--- src/routes/service-provider/services.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { ServiceManagement } from '@/components/service-provider/ServiceManagement';
-
-export const Route = createFileRoute('/service-provider/services')({
-  component: ServiceManagement,
-});
